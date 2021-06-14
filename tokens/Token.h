cf832c9ca27f2c90f736fa93c82d003e547d940b
Mateusz Kojro <mateuszkojro@outlook.com>
2021-06-14 02:41:11 +0200

    Some docks for token interface

--- tokens/Token.h
+++ tokens/Token.h
@@ -5,17 +5,23 @@
 #ifndef UI4_PROGRAMOWANIE_OBIEKTOWE_TOKEN_H
 #define UI4_PROGRAMOWANIE_OBIEKTOWE_TOKEN_H
 
-
+/// \brief Interface for all the Tokens encounter in the code
 class Token {
 public:
+
+    /// \brief Every token needs to know whats it type is
     enum TokenType {
         variable,
         function,
         scope
     };
 
+    /// Get the name used to access the token in the code
+    /// \return name of the tokne in code
     virtual const std::string &get_name() = 0;
 
+    /// Return the type of the token being accessed
+    /// \return the type of the token
     virtual TokenType get_type() = 0;
 };
 
