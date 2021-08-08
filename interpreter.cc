33c822f5cc2636c2eb71e7a9115be292eab43973
Mateusz Kojro <mateuszkojro@outlook.com>
2021-08-08 21:11:22 +0200

    Update main

--- interpreter.cc
+++ interpreter.cc
@@ -4,36 +4,42 @@
 
 #include "lisp/lisp.h"
 
-int run_interpreter(ls::Interpreter& i){
-    
+int run_interpreter(ls::Interpreter &i)
+{
+
     std::string line;
-    do {
-    
+    do
+    {
+
         std::getline(std::cin, line);
         i.interpret(line);
 
     } while (line != "q" || line == "quit");
-    
+
     return 0;
 }
 
-void print_help(){
+void print_help()
+{
     std::cout << "This is help!" << std::endl;
 }
 
 int main(int argc, char **argv)
 {
     ls::Interpreter i;
-    switch (argc){
-        case 1:
-            return run_interpreter(i);
-            break;
-        case 2:
-            return i.interpret(argv[1]);
-            break;
-        default:
-            print_help();
-            return 1;
-            break;
+    switch (argc)
+    {
+    case 1:
+        return run_interpreter(i);
+        break;
+
+    case 2:
+        return i.interpret(argv[1]);
+        break;
+
+    default:
+        print_help();
+        return 1;
+        break;
     }
 }
