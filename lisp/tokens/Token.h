74671a0e86ad2934138282421adf5fb3f00abe05
Mateusz Kojro <mateuszkojro@outlook.com>
2021-08-09 22:03:36 +0200

    Modifications to Token interface

--- lisp/tokens/Token.h
+++ lisp/tokens/Token.h
@@ -1,11 +1,24 @@
 #ifndef LS_TOKEN
 #define LS_TOKEN
 
+#include <iostream>
+
+#include "../Stack.h"
+
+#include "../utils/parsing.h"
+
+#define LOG_STATE(state) (std::cerr << state << std::endl)
+
 class Token
 {
 public:
     Token() = default;
     virtual ~Token() = default;
+
+    virtual Token *parse(char znak, Stack<Token *> &stack) = 0;
+
+protected:
+    std::string buffer_;
 };
 
 #endif
