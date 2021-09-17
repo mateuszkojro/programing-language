cf82f6530d858de959473cf7bd5d7e7c63813a7f
Mateusz Kojro <mateuszkojro@outlook.com>
2021-09-17 12:04:10 +0200

    Doxygen docs for BindDef

--- language/BindDef.h
+++ language/BindDef.h
@@ -10,6 +10,12 @@
 using std::optional;
 using std::string;
 
+/***
+ * @brief Variable binding statment
+ * @code
+ * mat x = 123
+ * @endcode
+ */
 class BindDef : public IStatment {
  public:
   static optional<pair<BindDef, string>> parse(const string &text);
@@ -20,10 +26,22 @@ class BindDef : public IStatment {
 
   bool operator==(const BindDef &op) const;
 
+  /***
+   * @brief Evaluation of Binding stores te value of the binding in current scope and
+   * return the evaluated value
+   * @param env Current scope
+   * @return Evaluated IValue
+   */
   IValue *eval(Env &env) override;
 
  private:
+  /***
+   * @brief Variable name
+   */
   string name_;
+  /***
+   * @brief Expression that will be evaluated to get variabe value
+   */
   IExpr *expr_;
 };
 
