4537af84d8b422ebc2dc11e88d7209fdb508f27c
Mateusz Kojro <mateuszkojro@outlook.com>
2021-09-17 22:35:04 +0200

    Doxygen  for IStatment

--- language/IStatment.h
+++ language/IStatment.h
@@ -7,18 +7,25 @@
 
 //#include "env.h"
 
-
 using std::optional;
 using std::pair;
 using std::string;
 
 class Env;
 
+/**
+ * @brief Base class for all possible statments in the language
+ */
 class IStatment {
  public:
   static optional<pair<IStatment *, string>> parse(const string &str);
   IStatment() = default;
 
+  /**
+   * @brief Function uesed to evaluate statment in the scope
+   * @param env Scope to evaluate statment in
+   * @return Object of the class deriving IValue
+   */
   virtual IValue *eval(Env &env) = 0;
 
   virtual ~IStatment() = default;
