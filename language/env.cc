0bce70c0022a09de2a9b51a167dfdfe4f655ac45
Mateusz Kojro <mateuszkojro@outlook.com>
2021-09-17 16:53:59 +0200

    Start refactoring Env

--- language/env.cc
+++ language/env.cc
@@ -11,9 +11,11 @@ optional<IValue *> Env::get_binding_value(const string &name) {
 	return std::nullopt;
   return bindings_[name];
 }
+
 void Env::store_function(const string &name, FuncDef *val) {
   functions_[name] = val;
 }
+
 optional<FuncDef *> Env::get_function_def(const string &name) {
   // There is no value with that name
   if (functions_.find(name) == functions_.end())
--- language/env.h
+++ language/env.h
@@ -1,29 +1,65 @@
+#ifndef ENV_H
+#define ENV_H
+
 #include "IStatment.h"
 #include "IValue.h"
 #include <optional>
 #include <string>
 #include <unordered_map>
 
-#ifndef ENV_H
-#define ENV_H
-
 using std::optional;
 using std::string;
 
 class FuncDef;
-
+/**
+ * @brief
+ */
 class Env {
  public:
+  /**
+   * @brief
+   * @param name
+   * @param val
+   */
   void store_binding(const string &name, IValue *val);
+
+  /**
+   * @brief
+   * @param name
+   * @param val
+   */
   void store_function(const string &name, FuncDef *val);
-  //  void store_definition(const string &name, IStatment *val);
+
+  /**
+   * @brief
+   * @param name
+   * @return
+   */
   optional<IValue *> get_binding_value(const string &name);
+
+  /**
+   * @brief
+   * @param name
+   * @return
+   */
   optional<FuncDef *> get_function_def(const string &name);
 
+  /**
+   * @brief
+   * @return
+   */
   std::unordered_map<string, IValue *> &get_bindings() { return bindings_; };
 
  private:
+
+  /**
+   * @brief
+   */
   std::unordered_map<string, IValue *> bindings_;
+
+  /**
+   * @brief
+   */
   std::unordered_map<string, FuncDef *> functions_;
 };
 
