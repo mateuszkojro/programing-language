183dbd92e24edf454f5d7a240500c77063e7da29
Mateusz Kojro <mateuszkojro@outlook.com>
2021-09-17 21:10:02 +0200

    Refactoring ErrorStatment

--- language/ErrorStatment.cc
+++ language/ErrorStatment.cc
@@ -1,16 +1,14 @@
-//
-// Created by mateu on 9/17/2021.
-//
-
 #include "ErrorStatment.h"
-#include <cassert>
+
 IValue *ErrorStatment::eval(Env &env) {
   return this;
 }
+
 double ErrorStatment::value() const {
-  assert(false && "Trying to extract value from the error");
+  FIXME("Error is implicitly converted to NaN here");
   return NAN;
 }
+
 std::string ErrorStatment::error() {
   return error_message_;
 }
--- language/ErrorStatment.h
+++ language/ErrorStatment.h
@@ -5,17 +5,40 @@
 
 #include "IStatment.h"
 #include "IValue.h"
+#include "utils.h"
 
+/**
+ * @brief Error value created when there is an error while evaluation
+ */
 class ErrorStatment : public IStatment, public IValue {
  public:
-  ErrorStatment(std::string error) : IValue(IValue::Type::Error), error_message_(std::move(error)) {}
+  explicit ErrorStatment(std::string error)
+	  : IValue(IValue::Type::Error),
+		error_message_(std::move(error)) {}
 
+  /**
+   * @brief Evaluation of the Error returns itself
+   * @param env Enviroment is ignored while evaluation
+   * @return Itself
+   */
   IValue *eval(Env &env) override;
+
+  /**
+   * @brief Value of error is NaN
+   * @return NaN
+   */
   double value() const override;
 
+  /**
+   * @brief Geter for error message
+   * @return Error message
+   */
   std::string error();
 
  private:
+  /**
+   * @brief Error message describing what happened
+   */
   std::string error_message_;
 };
 
