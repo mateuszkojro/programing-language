e21cb3e375e14e30f75425aaee916f636a5a9214
Mateusz Kojro <mateuszkojro@outlook.com>
2021-09-17 20:58:23 +0200

    Doxygen for Env

--- language/env.h
+++ language/env.h
@@ -12,53 +12,53 @@ using std::string;
 
 class FuncDef;
 /**
- * @brief
+ * @brief Representation of enviroment / scope in the languge
+ * (contains maps of function names -> function definitions and variable names -> variable definitions)
  */
 class Env {
  public:
   /**
-   * @brief
-   * @param name
-   * @param val
+   * @brief Store variable name with coresponding value
+   * @param name Name of a variable
+   * @param val Value for that variable
    */
   void store_binding(const string &name, IValue *val);
 
   /**
-   * @brief
-   * @param name
-   * @param val
+   * @brief Store function name with coresponding function definition
+   * @param name Name for the function
+   * @param val Function definition to be assigned
    */
   void store_function(const string &name, FuncDef *val);
 
   /**
-   * @brief
-   * @param name
-   * @return
+   * @brief Get value coresponding to variable name
+   * @param name Name of the variable
+   * @return Optional value
    */
   optional<IValue *> get_binding_value(const string &name);
 
   /**
-   * @brief
-   * @param name
-   * @return
+   * @brief Get Function Definition coresponding to the name
+   * @param name Name of the function
+   * @return Optional function definition
    */
   optional<FuncDef *> get_function_def(const string &name);
 
   /**
-   * @brief
-   * @return
+   * @brief Get a mutable reference to variable bindings
+   * @return Reference to variable bindings
    */
   std::unordered_map<string, IValue *> &get_bindings() { return bindings_; };
 
  private:
-
   /**
-   * @brief
+   * @brief Bindings variable name -> value
    */
   std::unordered_map<string, IValue *> bindings_;
 
   /**
-   * @brief
+   * @brief Bindings Function name -> function definitions
    */
   std::unordered_map<string, FuncDef *> functions_;
 };
